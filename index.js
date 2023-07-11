87274fcd182093cdb70270b3a61c9d37474c8f65
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 09:05:05 -0700

    Modify files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import  BookCollection  from './modules/bookCollection.js';
-import  initializeLinks  from './modules/linkHandler.js';
-import  initializeForm  from './modules/form.js';
-import { DateTime } from './modules/Luxon.js';
+import BookCollection from "./modules/bookCollection.js";
+import initializeLinks from "./modules/linkHandler.js";
+import initializeForm from "./modules/form.js";
+import { DateTime } from "./modules/Luxon.js";
 
-const dateContainer = document.getElementById('datetime-container');
-const dateContent = document.createElement('p');
-dateContent.textContent = `${DateTime.now().toLocaleString(DateTime.DATETIME_MED)}`;
+const dateContainer = document.getElementById("datetime-container");
+const dateContent = document.createElement("p");
+dateContent.textContent = `${DateTime.now().toLocaleString(
+  DateTime.DATETIME_MED
+)}`;
 dateContainer.appendChild(dateContent);
 const bookCollection = new BookCollection();
 initializeForm(bookCollection);
