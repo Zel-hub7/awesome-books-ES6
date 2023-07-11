28ee5be3edaebab97404219c6b27a8a31399eaa0
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 09:17:39 -0700

    Modify files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { DateTime } from './modules/Luxon.js';
 const dateContainer = document.getElementById('datetime-container');
 const dateContent = document.createElement('p');
 dateContent.textContent = `${DateTime.now().toLocaleString(
-  DateTime.DATETIME_MED
+  DateTime.DATETIME_MED,
 )}`;
 dateContainer.appendChild(dateContent);
 const bookCollection = new BookCollection();
