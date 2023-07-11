d4d4870a1168f2938eb0e959d94a865fc9d42c4d
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 02:37:41 -0700

    files updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { BookCollection } from './modules/bookCollection.js';
 
 const bookCollection = new BookCollection();
 
+
 const addBookForm = document.getElementById('add-book-form');
 addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -16,4 +17,17 @@ addBookForm.addEventListener('submit', (event) => {
   authorInput.value = '';
 });
 
+const bookCollectionLink = document.getElementById('all-books-link');
+const addLink = document.getElementById('add-book-link');
+const contact = document.getElementById('contact');
+const bookContainer = document.getElementById('all-books-container');
+const contactContainer = document.getElementById('contact');
+const AddForm = document.getElementById('add-book-container')
+AddForm.style.display = 'none';
+
+
+bookCollectionLink.addEventListener('click', function() {
+
+
+})
 bookCollection.loadFromLocalStorage();
