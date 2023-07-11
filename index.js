cc7dea8d5e8617bdf260fe926be6261345d63ffb
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 08:21:23 -0700

    Modify files

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ import { initializeLinks } from './modules/linkHandler.js';
 import { initializeForm } from './modules/form.js';
 
 
-
 let DateTime = luxon.DateTime;
-// Output: 2023-06-19T09:00:00.882+05:30 
-console.log(DateTime.now().toString());
 const dateContainer = document.getElementById('datetime-container');
 let dateContent  = document.createElement('p');
 dateContent.textContent = `${DateTime.now().toLocaleString(DateTime.DATETIME_MED)}`;
 dateContainer.appendChild(dateContent);
-
-
 const bookCollection = new BookCollection();
-
-
 initializeForm(bookCollection);
 bookCollection.loadFromLocalStorage();
 initializeLinks();
-
