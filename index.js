bf1ee8d21620d289c09fe4d0763f7a5482f153cf
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 05:10:16 -0700

    files modifed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { BookCollection } from './Modules/bookCollection.js';
+import { initializeLinks } from './Modules/linkHandler.js';
 
 const bookCollection = new BookCollection();
 
@@ -18,29 +19,5 @@ addBookForm.addEventListener('submit', (event) => {
 
 bookCollection.loadFromLocalStorage();
 
-// Add event listeners to the links
-const allBooksLink = document.getElementById('all-books-link');
-const addBookLink = document.getElementById('add-book-link');
-const contactLink = document.getElementById('contact');
-const allBooksContainer = document.getElementById('all-books-container');
-const addBookContainer = document.getElementById('add-book-container');
-const about = document.getElementById('about');
+initializeLinks();
 
-about.style.display = 'none';
-addBookContainer.style.display = 'none';
-allBooksLink.addEventListener('click', () => {
-  allBooksContainer.style.display = 'block'; // Show all books container
-  addBookContainer.style.display = 'none'; // Hide add book container
-  about.style.display = 'none';
-});
-
-addBookLink.addEventListener('click', () => {
-  allBooksContainer.style.display = 'none'; // Hide all books container
-  addBookContainer.style.display = 'block'; // Show add book container
-  about.style.display = 'none';
-});
-contactLink.addEventListener('click', () => {
-  about.style.display = 'block';
-  allBooksContainer.style.display = 'none';
-  addBookContainer.style.display = 'none';
-});
