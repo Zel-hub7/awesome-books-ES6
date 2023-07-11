50f134cbcc8d5aea21668e2ee6b4bc535dbbf0ce
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 06:19:46 -0700

    files Modified

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { BookCollection } from './Modules/bookCollection.js';
 import { initializeLinks } from './Modules/linkHandler.js';
 import { initializeForm } from './Modules/form.js';
+import { DateTimeComponent } from './Modules/DateTimeComponent.js';
 
 
-
+const date = new DateTimeComponent();
 const bookCollection = new BookCollection();
 
 
