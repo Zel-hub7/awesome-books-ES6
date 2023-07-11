1118e59fb868a197b48ed5400d66e0e4b42c0e7d
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-11 08:14:35 -0700

    Modify files

--- modules/form.js
+++ modules/form.js
@@ -1,15 +1,15 @@
-export const initializeForm = (bookCollection)=> {
-    const addBookForm = document.getElementById('add-book-form');
-    addBookForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const titleInput = document.getElementById('title');
-      const authorInput = document.getElementById('author');
-      const title = titleInput.value;
-      const author = authorInput.value;
-    
-      bookCollection.addBook(title, author);
-    
-      titleInput.value = '';
-      authorInput.value = '';
-    });
-}
+export const initializeForm = (bookCollection) => {
+  const addBookForm = document.getElementById("add-book-form");
+  addBookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    const title = titleInput.value;
+    const author = authorInput.value;
+
+    bookCollection.addBook(title, author);
+
+    titleInput.value = "";
+    authorInput.value = "";
+  });
+};
